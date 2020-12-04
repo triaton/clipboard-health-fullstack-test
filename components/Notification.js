115889060256a224181b7b6fbb7a4a08bd2f7f38
triaton <triaton00@gmail.com>
2020-12-04 10:59:57 +0300

    chore: error response test

--- components/Notification.js
+++ components/Notification.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Notification = ({ color, type, message, onClose }) => {
   return (
-    <div className="fixed right-5 top-5 z-20">
+    <div className="fixed right-5 top-5 z-30">
       <div className={`text-white px-6 py-4 border-0 rounded relative mb-4 bg-${color}-500`}>
         <span className="inline-block align-middle mr-8">
           <b className="capitalize">{type}</b> {message}
--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -51,6 +51,7 @@ export default async function queryJobs(req, res) {
   // you need to figure out how to guarantee that client side will render
   // correct results even if server-side can't finish replies in the right order
   await new Promise((resolve)=>setTimeout(resolve, 1000 * Math.random()));
-  res.json(jobsData);
+  // res.json(jobsData);
+  res.error('hello');
   return jobsData;
 }
